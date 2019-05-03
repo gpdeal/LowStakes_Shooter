0da449849b57bed805040cb54df4da2ef349346f
Galkar <gpdeal@gmail.com>
2019-05-02 22:18:26 -0700

    add hose particles

---
+++ Assets/Scripts/Randy/PlayerShooting.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    private ParticleSystem hoseParticles;
+    private bool firing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hoseParticles = GetComponentInChildren<ParticleSystem>();
+        firing = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateHoseLevel();
+    }
+
+    private void UpdateHoseLevel()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            // increase speed and rate of hose particles
+            hoseParticles.startSpeed = 60;
+            ParticleSystem.EmissionModule emission = hoseParticles.emission;
+            emission.rateOverTime = 300;
+        } else
+        {
+            // decrease speed and rate of hose particles
+            hoseParticles.startSpeed = 3;
+            ParticleSystem.EmissionModule emission = hoseParticles.emission;
+            emission.rateOverTime = 20;
+        }
+    }
+}
